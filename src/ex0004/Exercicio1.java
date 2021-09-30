9a6b394ad56cbc0b406f0b531525d48b8005b317
Gracielle Martins <gracimtns@gmail.com>
2021-09-30 13:02:53 -0300

    Add ex0004

---
+++ src/ex0004/Exercicio1.java
@@ -0,0 +1,21 @@
+package ex0004;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		int a, b, prod;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		prod = a * b;
+		
+		System.out.println("PROD = " + prod);
+
+	}
+
+}
