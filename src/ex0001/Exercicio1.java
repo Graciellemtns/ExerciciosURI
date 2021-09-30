df2fcfd9cfd00f731e02449e3517ba07ef929ffe
Gracielle Martins <gracimtns@gmail.com>
2021-09-30 15:08:07 -0300

    add ex0001

---
+++ src/ex0001/Exercicio1.java
@@ -0,0 +1,20 @@
+package ex0001;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		int a, b, x;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		x = a + b;
+		
+		System.out.println("X = " + x);
+	}
+
+}
