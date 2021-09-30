b911364252a8c42622f65d4dd40ee602de9f8996
Gracielle Martins <gracimtns@gmail.com>
2021-09-30 14:55:15 -0300

    Add ex0002

---
+++ src/ex0002/Exercicio2.java
@@ -0,0 +1,27 @@
+package ex0002;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner (System.in);
+
+		
+		double raio;
+		double area;
+	
+		raio = sc.nextDouble();
+		
+		area = raio * raio * 3.14159;
+		
+		DecimalFormat df = new DecimalFormat(".####");
+		
+		System.out.format("A =  " +  df.format(area));
+
+	}
+
+}
