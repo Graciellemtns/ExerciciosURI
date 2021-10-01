efa3a095376d981964990c4c9a7520309df46727
Gracielle Martins <gracimtns@gmail.com>
2021-10-01 13:43:06 -0300

    Ad ex1007

---
+++ src/ex1007/Exercicio07.java
@@ -0,0 +1,25 @@
+package ex1007;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio07 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		int a, b, c, d, diferenca;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		d = sc.nextInt();
+		
+		diferenca = (a * b -c *d);
+		
+		System.out.println("DIFERENCA = " + diferenca);
+		
+
+	}
+
+}
